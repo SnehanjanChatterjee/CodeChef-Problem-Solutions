9a431eea973b78980b419ed237b3603f7d4fc071
Snehanjan Chatterjee <41839617+SnehanjanChatterjee@users.noreply.github.com>
2019-07-14 20:16:26 +0530

    Create Dignity of the Nation.cpp

---
+++ Fool You 2019/Dignity of the Nation/Dignity of the Nation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long long int n;
+	  cin>>n;
+	  if(n==81 || n==1458 || n==1 || n==1729)
+	      cout<<"elegant"<<endl;
+	  else
+	      cout<<"not elegant"<<endl;
+	  return 0;
+}
